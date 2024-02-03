0a3b877ba15e4faca189364c30701d4852e7ad8e
ThomasPijper <s1141675@student.hsleiden.nl>
2024-02-03 15:06:29 +0100

    Initial commit

---
+++ src/main/java/com/s1141775/iprwc_g2_backend/IprwcG2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.s1141775.iprwc_g2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcG2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IprwcG2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/s1141775/iprwc_g2_backend/IprwcG2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1141775.iprwc_g2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IprwcG2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
